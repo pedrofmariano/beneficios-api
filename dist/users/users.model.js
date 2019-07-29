bfd4bd1e22c89a1b361bf4a73658a0fdb3cec353
pedrofxx2 <pedroh.fortunatos@gmail.com>
2019-07-28 22:08:47 -0300

    first commit

---
+++ dist/users/users.model.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String
+    }
+});
+exports.User = mongoose.model('User', userSchema);
